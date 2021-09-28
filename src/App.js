f7a22a615685945a214e26e80dba18c59fae8f49
Aditi0505 <40437427+Aditi0505@users.noreply.github.com>
2021-09-29 02:53:40 +0530

    style: Add css styling

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import { useState } from "react";
 import "./styles.css";
 
 const emojiDictonary = {
-  "♈": "Aries",
   "♉": "Taurus",
+  "♈": "Aries",
+  "♋": "Cancer",
   "♌": "Leo",
-  "♓": "Pisces"
+  "♓": "Pisces",
+  "♐": "Sagittarius",
+  "♏": "Scorpio",
+  "♒": "Aquarius",
+  "♍": "Virgo",
+  "♊": "Gemini",
+  "♑": "Capricorn",
+  "♎": "libra"
 };
 
 const emojiKeys = Object.keys(emojiDictonary);
 export default function App() {
-  const [emojiMeaning, setEmojiMeaning] = useState("");
+  const [emojiMeaning, setEmojiMeaning] = useState(
+    "Your meaning will appear here!"
+  );
 
   function onChangeHandler(event) {
     var meaning = emojiDictonary[event.target.value];
     if (meaning === undefined) {
-      meaning = "This is not present in our database!";
+      meaning = "not present in our database!";
     }
     setEmojiMeaning(meaning);
   }
@@ -26,11 +36,21 @@ export default function App() {
   return (
     <div className="App">
       <h1>Zodiac Emoji...</h1>
-      <input onChange={onChangeHandler} />
-      <div style={{ margin: "0.8rem" }}>{emojiMeaning}</div>
+      <input
+        onChange={onChangeHandler}
+        placeholder="Enter the symbol"
+        style={{ width: "80vw", height: "5vh" }}
+      />
+      <div style={{ margin: "0.8rem", padding: "1rem", fontWeight: "bolder" }}>
+        {emojiMeaning}
+      </div>
       <div style={{ fontSize: "2rem" }}>
         {emojiKeys.map((emoji) => (
-          <span key={emoji} onClick={() => onClickHandler(emoji)}>
+          <span
+            key={emoji}
+            onClick={() => onClickHandler(emoji)}
+            style={{ cursor: "pointer", padding: "1rem" }}
+          >
             {emoji}
           </span>
         ))}
